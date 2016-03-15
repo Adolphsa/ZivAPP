4ee48846360cc1aa736c830659fd04157508d852
Adolphsa <497749990@qq.com>
2016-03-15 17:37:52 +0800

    2016/03/14 初步完成车辆信息页面的布局

--- app/src/main/java/com/zividig/zivapp/tabs/CarInfo.java
+++ app/src/main/java/com/zividig/zivapp/tabs/CarInfo.java
@@ -1,10 +1,12 @@
 package com.zividig.zivapp.tabs;
 
-import android.app.Activity;
 import android.app.Application;
-import android.graphics.Color;
-import android.view.Gravity;
-import android.widget.TextView;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import com.zividig.zivapp.R;
 
 /**
  * 车辆信息
@@ -13,18 +15,54 @@ import android.widget.TextView;
 public class CarInfo extends BasePager {
 
 
+    private  View view;
+    private  ImageView speedPoint;
+    private  ImageView voltagePoint;
+    private  ImageView turnSpeedPoint;
+    private  ImageView temperaturePoint;
+
     public CarInfo(Application application) {
         super(application);
+
+        view = View.inflate(application, R.layout.activity_carinfo, null);
+        speedPoint = (ImageView) view.findViewById(R.id.img_speed_point);
+        voltagePoint = (ImageView) view.findViewById(R.id.img_voltage_point);
+        turnSpeedPoint = (ImageView) view.findViewById(R.id.img_turn_speed_point);
+        temperaturePoint = (ImageView) view.findViewById(R.id.img_temperature_point);
     }
 
     @Override
     public void initData() {
-        TextView text = new TextView(mApplication);
-        text.setText("车辆信息");
-        text.setTextSize(20);
-        text.setTextColor(Color.RED);
-        text.setGravity(Gravity.CENTER);
 
-        basePager_fl.addView(text);
+        basePager_fl.removeAllViews(); //必须先移除所有的view,否则会报错
+        basePager_fl.addView(view);
+
+        initAnimation();
+    }
+
+    public void initAnimation(){
+        //速度动画
+        RotateAnimation speedRotate = new RotateAnimation(0,-110, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        speedRotate.setDuration(500);
+        speedRotate.setFillAfter(true);
+        speedPoint.setAnimation(speedRotate);
+
+        //电压动画
+        RotateAnimation voltageRotate = new RotateAnimation(0,65, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.8f);
+        voltageRotate.setDuration(500);
+        voltageRotate.setFillAfter(true);
+        voltagePoint.setAnimation(voltageRotate);
+
+        //转速动画
+        RotateAnimation turnRotate = new RotateAnimation(0,-130, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        turnRotate.setDuration(500);
+        turnRotate.setFillAfter(true);
+        turnSpeedPoint.setAnimation(turnRotate);
+
+        //水温动画
+        RotateAnimation temperatureRotate = new RotateAnimation(0,-70, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.8f);
+        temperatureRotate.setDuration(500);
+        temperatureRotate.setFillAfter(true);
+        temperaturePoint.setAnimation(temperatureRotate);
     }
 }
