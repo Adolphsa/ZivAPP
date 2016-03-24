856388403b75acfc2dd4ee6244ef837041c10ab0
Adolphsa <497749990@qq.com>
2016-03-24 18:57:48 +0800

    2016/03/22  点击加油站移到中心点，并显示详情

--- app/src/main/java/com/zividig/zivapp/baidumap/GasStation.java
+++ app/src/main/java/com/zividig/zivapp/baidumap/GasStation.java
@@ -1,6 +1,7 @@
 package com.zividig.zivapp.baidumap;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,17 +13,21 @@ import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.InfoWindow;
 import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
-import com.baidu.mapapi.map.Overlay;
+import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.model.LatLng;
 import com.baidu.mapapi.search.core.PoiInfo;
 import com.baidu.mapapi.search.core.SearchResult;
 import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
-import com.baidu.mapapi.search.poi.PoiCitySearchOption;
 import com.baidu.mapapi.search.poi.PoiDetailResult;
 import com.baidu.mapapi.search.poi.PoiDetailSearchOption;
 import com.baidu.mapapi.search.poi.PoiNearbySearchOption;
@@ -32,7 +37,7 @@ import com.zividig.zivapp.R;
 import com.zividig.zivapp.overlayutil.PoiOverlay;
 
 /**
- * 加油站
+ * 加油站检索
  * Created by Administrator on 2016-03-23.
  */
 public class GasStation extends Activity {
@@ -45,6 +50,8 @@ public class GasStation extends Activity {
     private LatLng ll;
     private boolean isFirstLocation = true;
     private PoiSearch poiSearch;
+    private Marker marker;
+    PoiOverlay overlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,8 +76,11 @@ public class GasStation extends Activity {
         mapView.showZoomControls(false);
         baiduMap = mapView.getMap();
 
+        poiSearch = PoiSearch.newInstance(); //生成检索对象
+        poiSearch.setOnGetPoiSearchResultListener(poiSearchListener);
+
         initLocation();
-        initPOI();
+
     }
 
     //初始化定位数据
@@ -84,6 +94,7 @@ public class GasStation extends Activity {
         myListener = new MyLocationListener();
         mLocationClient.registerLocationListener(myListener);
 
+        //设置定位参数
         LocationClientOption option = new LocationClientOption();
         option.setOpenGps(true); // 打开gps
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//高精度
@@ -93,55 +104,101 @@ public class GasStation extends Activity {
         option.setNeedDeviceDirect(true);//返回的定位结果包含手机机头的方向
         mLocationClient.setLocOption(option);
         mLocationClient.start();
+
+
+        if (ll != null){
+            System.out.println("执行检索");
+            nearSearch(ll);
+        }
+
     }
 
-    public void initPOI(){
-        poiSearch = PoiSearch.newInstance();
-        //设置POI检索监听者
-        poiSearch.setOnGetPoiSearchResultListener(new OnGetPoiSearchResultListener() {
-            @Override
-            public void onGetPoiResult(PoiResult poiResult) {
-                System.out.println("检索结果");
-                //获取POI检索结果
-                String error = poiResult.error.toString();
-                System.out.println(error);
-
-                if (poiResult == null
-                        || poiResult.error == SearchResult.ERRORNO.RESULT_NOT_FOUND) {
-                    Toast.makeText(getApplicationContext(), "未找到结果", Toast.LENGTH_LONG)
-                            .show();
-                    return;
-                }
-
-                if (poiResult.error == SearchResult.ERRORNO.NO_ERROR) {
-                    baiduMap.clear();
-                    PoiOverlay overlay = new MyPoiOverlay(baiduMap);
-                    baiduMap.setOnMarkerClickListener(overlay);
-                    overlay.setData(poiResult);
-                    overlay.addToMap();
-                    overlay.zoomToSpan();
-                    System.out.println("检索结果执行了");
-                }
-
-            }
-
-            @Override
-            public void onGetPoiDetailResult(PoiDetailResult poiDetailResult) {
-                //获取Place详情页检索结果
-                if (poiDetailResult.error != SearchResult.ERRORNO.NO_ERROR) {
-                    Toast.makeText(getApplicationContext(), "抱歉，未找到结果",
-                            Toast.LENGTH_SHORT).show();
-                } else {// 正常返回结果的时候，此处可以获得很多相关信息
-                    Toast.makeText(
-                            getApplicationContext(),
-                            poiDetailResult.getName() + ": "
-                                    + poiDetailResult.getAddress(),
-                            Toast.LENGTH_LONG).show();
-                }
-
-            }
-        });
+    //发起附近检索
+    public void nearSearch(LatLng ll){
+        //发起附近检索
+        PoiNearbySearchOption nearbySearchOption = new PoiNearbySearchOption();
+        System.out.println(ll);
+        nearbySearchOption.location(ll);
+        nearbySearchOption.keyword("加油站");
+        nearbySearchOption.radius(5000);// 检索半径，单位是米
+        nearbySearchOption.pageCapacity(10);
+        nearbySearchOption.pageNum(1);
+        poiSearch.searchNearby(nearbySearchOption);// 发起附近检索请求
     }
+
+    //检索监听
+    OnGetPoiSearchResultListener poiSearchListener = new OnGetPoiSearchResultListener(){
+
+        @Override
+        public void onGetPoiResult(PoiResult poiResult) {
+            System.out.println("检索结果");
+            //获取POI检索结果
+            String error = poiResult.error.toString();
+            System.out.println(error);
+
+            if (poiResult == null
+                    || poiResult.error == SearchResult.ERRORNO.RESULT_NOT_FOUND) {
+                Toast.makeText(getApplicationContext(), "未找到结果", Toast.LENGTH_LONG)
+                        .show();
+                return;
+            }
+
+            if (poiResult.error == SearchResult.ERRORNO.NO_ERROR) { //返回结果正常
+                baiduMap.clear();
+                overlay = new MyPoiOverlay(baiduMap);
+                baiduMap.setOnMarkerClickListener(overlay);
+                overlay.setData(poiResult);
+                overlay.addToMap();
+                overlay.zoomToSpan();
+                System.out.println("检索结果执行了");
+
+            }
+        }
+
+        @Override
+        public void onGetPoiDetailResult(final PoiDetailResult poiDetailResult) {
+            //获取Place详情页检索结果
+            if (poiDetailResult.error != SearchResult.ERRORNO.NO_ERROR) {
+                Toast.makeText(getApplicationContext(), "抱歉，未找到结果",
+                        Toast.LENGTH_SHORT).show();
+            } else {// 正常返回结果的时候，此处可以获得很多相关信息
+
+                baiduMap.setOnMarkerClickListener(new BaiduMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+
+
+                        System.out.println("Marker被点击了");
+
+                        //移到中心点
+                        MapStatus.Builder builder = new MapStatus.Builder();
+                        builder.target(marker.getPosition());
+                        baiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+
+                        View view = View.inflate(getApplicationContext(),R.layout.popwindow,null);
+                        TextView text = (TextView) view.findViewById(R.id.tv_popwindow);
+
+                        //设置要显示的信息
+                        text.setText(poiDetailResult.getName());
+                        InfoWindow.OnInfoWindowClickListener listener = null;
+                        listener = new InfoWindow.OnInfoWindowClickListener() {
+                            @Override
+                            public void onInfoWindowClick() {
+                                System.out.println("InfoWinsow被点击");
+                                baiduMap.hideInfoWindow();
+                            }
+                        };
+                        LatLng ll = marker.getPosition();
+                        InfoWindow mInfoWindow = new InfoWindow(BitmapDescriptorFactory.fromView(text), ll, -47, listener);
+                        baiduMap.showInfoWindow(mInfoWindow);
+                        return true;
+                    }
+                });
+
+            }
+        }
+    };
+
 
     //继承POI工具类中的PoiOverlay
     class MyPoiOverlay extends PoiOverlay {
@@ -193,21 +250,8 @@ public class GasStation extends Activity {
             }
 
             //发起附近检索
-            PoiNearbySearchOption nearbySearchOption = new PoiNearbySearchOption();
-            nearbySearchOption.location(ll);
-            nearbySearchOption.keyword("加油站");
-            nearbySearchOption.radius(5000);// 检索半径，单位是米
-            nearbySearchOption.pageCapacity(10);
-            nearbySearchOption.pageNum(1);
-            poiSearch.searchNearby(nearbySearchOption);// 发起附近检索请求
+            nearSearch(ll);
 
-//            PoiCitySearchOption citySearchOption = new PoiCitySearchOption();
-//            citySearchOption.city(bdLocation.getCity());// 城市
-//            citySearchOption.keyword("加油站");// 关键字
-//            citySearchOption.pageCapacity(15);// 默认每页10条
-//            citySearchOption.pageNum(1);// 分页编号
-//            // 发起检索请求
-//            poiSearch.searchInCity(citySearchOption);
         }
     }
 
@@ -242,6 +286,7 @@ public class GasStation extends Activity {
     public void onDestroy() {
         // 退出时销毁定位
         mLocationClient.stop();
+
         // 关闭定位图层
         baiduMap.setMyLocationEnabled(false);
         mapView.onDestroy();
