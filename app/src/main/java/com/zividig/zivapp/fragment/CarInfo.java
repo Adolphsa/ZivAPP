7315db43e1d19922dc1b0e266d1d4d2645dbb92c
Adolphsa <497749990@qq.com>
2016-04-06 11:47:43 +0800

    2016/04/06  加入定时器让速度指针定时转动

--- app/src/main/java/com/zividig/zivapp/fragment/CarInfo.java
+++ app/src/main/java/com/zividig/zivapp/fragment/CarInfo.java
@@ -1,6 +1,8 @@
 package com.zividig.zivapp.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,7 +15,8 @@ import android.widget.TextView;
 
 import com.zividig.zivapp.R;
 
-import java.security.Principal;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * 车辆信息
@@ -22,88 +25,107 @@ import java.security.Principal;
 public class CarInfo extends Fragment {
 
     private ImageView speedPoint;
-    private  ImageView voltagePoint;
+    private  ImageView oilPoint;
     private  ImageView turnSpeedPoint;
     private  ImageView temperaturePoint;
     private RotateAnimation speedRotate; //速度动画
-    private int [] speedTest = {10,50,100,80,120,60,30,10,50};
+    private int [] speedTest = {10,50,100,80,120,60,30,150,50};
     private int i;
     private int xStart,xEnd;
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 0){
+                int n = i%9;
+                System.out.println("接收到消息了");
+                speedRotate = new RotateAnimation(0,speedTest[n], Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+                speedRotate.setDuration(1000);
+                speedRotate.setFillAfter(true);
+                speedRotate.setRepeatCount(Animation.INFINITE);
+                speedPoint.setAnimation(speedRotate);
+                i++;
+            }
+        }
+    };
+    private Timer timer;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.car_information,container,false);
 
-//        speedPoint = (ImageView) view.findViewById(R.id.img_speed_point);
-//        voltagePoint = (ImageView) view.findViewById(R.id.img_voltage_point);
-//        turnSpeedPoint = (ImageView) view.findViewById(R.id.img_turn_speed_point);
-//        temperaturePoint = (ImageView) view.findViewById(R.id.img_temperature_point);
+        speedPoint = (ImageView) view.findViewById(R.id.img_speed_point);
+        oilPoint = (ImageView) view.findViewById(R.id.img_oil_point);
+        turnSpeedPoint = (ImageView) view.findViewById(R.id.img_turning_point);
+        temperaturePoint = (ImageView) view.findViewById(R.id.img_temperature_point);
 
         //设置标题
         TextView title = (TextView)view.findViewById(R.id.tv_title);
         title.setText("车辆信息");
 
-//        initView();
+
+        initView();
         return view;
     }
 
     public void initView() {
 
         initAnimation();
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("执行一次");
+                mHandler.sendEmptyMessage(0);
+            }
+        }, 0, 1000);
     }
 
     public void initAnimation(){
 
         System.out.println("动画被调用");
         //速度动画
-        speedRotate = new RotateAnimation(0,110, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        speedRotate = new RotateAnimation(0,speedTest[0], Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
         speedRotate.setDuration(1000);
-        speedRotate.setFillAfter(false);
+        speedRotate.setFillAfter(true);
         speedRotate.setRepeatCount(Animation.INFINITE);
         speedPoint.setAnimation(speedRotate);
 
 
-        //电压动画
-        RotateAnimation voltageRotate = new RotateAnimation(0,65, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.8f);
-        voltageRotate.setDuration(500);
-        voltageRotate.setFillAfter(true);
-        voltagePoint.setAnimation(voltageRotate);
+        //油压动画
+        RotateAnimation oilRotate = new RotateAnimation(0,100, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        oilRotate.setDuration(500);
+        oilRotate.setFillAfter(true);
+        oilPoint.setAnimation(oilRotate);
 
-//        //转速动画
-//        RotateAnimation turnRotate = new RotateAnimation(0,-130, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-//        turnRotate.setDuration(500);
-//        turnRotate.setFillAfter(true);
-//        turnSpeedPoint.setAnimation(turnRotate);
+        //转速动画
+        RotateAnimation turnRotate = new RotateAnimation(0,110, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        turnRotate.setDuration(800);
+        turnRotate.setFillAfter(false);
+        turnRotate.setRepeatCount(Animation.INFINITE);
+        turnSpeedPoint.setAnimation(turnRotate);
 
         //水温动画
-        RotateAnimation temperatureRotate = new RotateAnimation(0,-70, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.8f);
+        RotateAnimation temperatureRotate = new RotateAnimation(0,60, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
         temperatureRotate.setDuration(500);
         temperatureRotate.setFillAfter(true);
         temperaturePoint.setAnimation(temperatureRotate);
     }
 
 
-    public void speedAnimation(int xStart,int xEnd){
-        speedRotate = new RotateAnimation(xStart,xEnd, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-        speedRotate.setDuration(500);
-        speedRotate.setFillAfter(true);
-        speedPoint.setAnimation(speedRotate);
-        xStart = xEnd;
-        xEnd = speedTest[i];
-        i++;
-    }
-
     @Override
     public void onHiddenChanged(boolean hidden) {
         super.onHiddenChanged(hidden);
         if (hidden){ //隐藏
             System.out.println("车辆信息隐藏了");
-
+            timer.cancel();
+            timer = null;
 
         }else { //显示
             System.out.println("车辆信息显示了");
             initView();
+
         }
     }
 }
